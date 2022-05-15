f618261ec3ccce149e3c09d86af5a7475a9a5483
sundada <956593305@qq.com>
2022-05-16 07:17:55 +0800

    add parse three type and throw error when lack end

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -6,31 +6,69 @@ const enum TagType {
 
 export function baseParse (content: string) {
   const context = createParseContext(content)
-  return createRoot(parseChildren(context))
+  return createRoot(parseChildren(context, []))
 }
 
-function parseChildren (context) {
+function parseChildren (context, ancestor) {
   const nodes: any[] = []
-  let node
-  if (context.source.startsWith('{{')) {
-    node = parseInterpolation(context)
-  } else if (context.source[0] === '<') {
-    if (/[a-z]/i.test(context.source[1])) {
-      // element
-      node = parseElement(context)
+  while (!isEnd(context, ancestor)) {
+    let node
+
+    // 差值表达式
+    if (context.source.startsWith('{{')) {
+      node = parseInterpolation(context)
+    } else if (context.source[0] === '<') {
+      // 标签
+      if (/[a-z]/i.test(context.source[1])) {
+        // element
+        node = parseElement(context, ancestor)
+      }
     }
+    // text 文本
+    if (!node) {
+      // handle text
+      node = parseText(context)
+    }
+    nodes.push(node)
   }
-  if (!node) {
-    // handle text
-    node = parseText(context)
-  }
-  nodes.push(node)
   return nodes
 }
 
+function isEnd (context, ancestor) {
+  const s = context.source
+  // 2. 当遇到结束标签的时候
+  if (s.startsWith('</')) {
+    // 有个问题就是只要结束标签和之前的所有开始标签有一个可以匹配上跳出循环
+    // 所以抛出标签不匹配的逻辑应该 parseElement 的时候做
+    for (let i = ancestor.length - 1; i >= 0; i--) {
+      const tag = ancestor[i].tag
+      // if (s.slice(2, 2 + tag.length) === tag) {
+      //   return true
+      // }
+      if (startsWithEndTagOpen(s, tag)) {
+        return true
+      }
+    }
+  }
+
+  // if (parentTag && s.startsWith(`</${parentTag}>`)) {
+  //   return true
+  // }
+  // 1. source 有值的时候
+  return !s
+}
+
 function parseText (context: any) {
+  let endIndex = context.source.length
+  let endTokens = ['<', '{{']
+  for (let i = 0; i < endTokens.length; i++) {
+    const index = context.source.indexOf(endTokens[i])
+    if (index !== -1 && endIndex > index) {
+      endIndex = index
+    }
+  }
   // 1. 获取content
-  const content = parseTextData(context, context.source.length)
+  const content = parseTextData(context, endIndex)
   return {
     type: NodeTypes.TEXT,
     content
@@ -44,13 +82,32 @@ function parseTextData (context: any, length) {
   return content
 }
 
-function parseElement (context: any) {
+function parseElement (context: any, ancestor) {
   // 1. 解析 tag
-  const element = parseTag(context, TagType.START)
+  const element: any = parseTag(context, TagType.START)
+
+  ancestor.push(element)
+
+  element.children = parseChildren(context, ancestor)
+
+  // 从 ancestor 中抛出标签，应该在判断 ancestor 中的末尾标签和需要结束标签是不是同一个标签
+  // if (context.source.slice(2, 2 + element.tag.length) === element.tag) {
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    ancestor.pop()
+  } else {
+    throw new Error(`缺少结束标签:${element.tag}`)
+  }
 
   parseTag(context, TagType.END)
 
   return element
+}
+
+function startsWithEndTagOpen (source, tag) {
+  return (
+    source.startsWith('</') &&
+    source.slice(2, 2 + tag.length).toLowerCase() === tag
+  )
 }
 
 function parseTag (context: any, type: TagType) {
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -20,11 +20,8 @@ describe('parse', () => {
       // root
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.ELEMENT,
-        tag: 'div'
-        // content: {
-        //   type: NodeTypes.SIMPLE_EXPRESSION,
-        //   content: 'message'
-        // }
+        tag: 'div',
+        children: []
       })
     })
   })
@@ -37,5 +34,56 @@ describe('parse', () => {
         content: 'some text'
       })
     })
+  })
+  test('hello world', () => {
+    const ast = baseParse('<p>hi,{{message}}</p>')
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'p',
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: 'hi,'
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message'
+          }
+        }
+      ]
+    })
+  })
+  test('Nested element', () => {
+    const ast = baseParse('<div><p>hi</p>{{message}}</div>')
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.ELEMENT,
+          tag: 'p',
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: 'hi'
+            }
+          ]
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message'
+          }
+        }
+      ]
+    })
+  })
+  test('should throw error when lack and tag', () => {
+    expect(() => {
+      baseParse('<div><span></div>')
+    }).toThrow('缺少结束标签:span')
   })
 })
