2f3b61ee24ec98b3cf5b1da754bdfafa70f404f5
sundada <956593305@qq.com>
2022-05-16 08:49:40 +0800

    implement transform

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,40 @@
+export function transform (root, options) {
+  const context = createTransformContext(root, options)
+
+  // 1. 遍历 - 深度优先遍历
+  traverseNode(root, context)
+  // 2. 修改 text content
+}
+
+function createTransformContext (root: any, options: any) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || []
+  }
+  return context
+}
+
+function traverseNode (node: any, context) {
+  // 直接写死不够灵活 => 采用插件体系
+  // if (node.type === NodeTypes.TEXT) {
+  //   node.content += ' mini-vue'
+  // }
+  const nodeTransform = context.nodeTransforms
+
+  for (let i = 0; i < nodeTransform.length; i++) {
+    const transform = nodeTransform[i]
+    transform(node)
+  }
+
+  // handle element ...
+  transformChildren(node, context)
+}
+function transformChildren (node: any, context: any) {
+  const children = node.children
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      traverseNode(node, context)
+    }
+  }
+}
